d7e08e7ef93421e067239d26feeaba911d997f76
Saulo Henrique Rodrigues <saulohrodrigues@gmail.com>
2025-12-29 13:54:37 -0300

    chore: Estrutura inicial do projeto;

---
+++ src/main/java/com/desafio_pessoas02/Pessoa02/Pessoa02Application.java
@@ -0,0 +1,13 @@
+package com.desafio_pessoas02.Pessoa02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pessoa02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pessoa02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio_pessoas02/Pessoa02/Pessoa02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio_pessoas02.Pessoa02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pessoa02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
